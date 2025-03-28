dbf05b7f8273fbd7a9412e52aa3a715826da0ea5
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-03-28 16:02:33 -0400

    Adding codes using MongoDB

--- aprendendo.js
+++ aprendendo.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/aprendendo", );
-
-
+mongoose.connect("mongodb://localhost/bancoqualquer").then(() => {
+    console.log("Conectado com sucesso");
+}).catch((erro) => {
+    console.log("Erro ao conectar" + erro);
+});
